6ed7ff66b58b9a29b398c30fbbeacfb58adf4b73
adoherty8 <azdoherty91@gmail.com>
2019-04-06 11:41:53 -0400

    make lookup layout horizontal

--- static/js/charting.js
+++ static/js/charting.js
@@ -1,19 +1,6 @@
 
-var base_url = window.location.href;
-
-// verify jquery is working
-// window.onload = function()
-// {
-//     if (window.jQuery)
-//     {
-//         alert('jQuery is loaded');
-//     }
-//     else
-//     {
-//         alert('jQuery is not loaded');
-//     }
-// }
-
-$('.btn').on('click', function (e) {
-    console.log("Thing clicked");
+$('#pltbtn').on('click', function (e) {
+    symbol = $("#symbol").eq(0).value;
+    console.log(symbol);
+    
 })
